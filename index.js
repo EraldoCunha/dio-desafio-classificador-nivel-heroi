85e457a80a0ba946a4030b3993551fcf31342207
Eraldo Cunha <eraldo_cunha@hotmail.com>
2024-10-31 15:04:13 -0300

    Implementando laco de repeticao

--- index.js
+++ index.js
@@ -5,34 +5,49 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Digite o nome do herói: ", (nome) => {
-    rl.question("Digite a quantidade de experiência (XP) do herói: ", (xpInput) => {
-        let xp = parseInt(xpInput) || 0;
+function determinarNivel(xp) {
+    if (xp < 1000) return "Ferro";
+    else if (xp <= 2000) return "Bronze";
+    else if (xp <= 5000) return "Prata";
+    else if (xp <= 7000) return "Ouro";
+    else if (xp <= 8000) return "Platina";
+    else if (xp <= 9000) return "Ascendente";
+    else if (xp <= 10000) return "Imortal";
+    else return "Radiante";
+}
 
-        function determinarNivel(xp) {
-            switch (true) {
-                case xp < 1000:
-                    return "Ferro";
-                case xp <= 2000:
-                    return "Bronze";
-                case xp <= 5000:
-                    return "Prata";
-                case xp <= 7000:
-                    return "Ouro";
-                case xp <= 8000:
-                    return "Platina";
-                case xp <= 9000:
-                    return "Ascendente";
-                case xp <= 10000:
-                    return "Imortal";
-                default:
-                    return "Radiante";
-            }
-        }
-
-        let nivel = determinarNivel(xp);
-        console.log(`O Herói de nome ${nome} está no nível de ${nivel}.`);
-
-        rl.close();
+function perguntarContinuar() {
+    return new Promise((resolve) => {
+        rl.question("Deseja classificar mais heróis? (s/n): ", (resposta) => {
+            resolve(resposta.toLowerCase() === 's');
+        });
     });
-});
+}
+
+async function classificarHerois() {
+    let continuar = true; 
+
+    while (continuar) {
+        const nome = await new Promise((resolve) => {rl.question("Digite o nome do herói: ", resolve)});
+
+        const xpInput = await new Promise((resolve) => {rl.question("Digite a quantidade de experiência (XP) do herói: ", resolve)});
+
+        let xp = parseInt(xpInput);
+
+         if (isNaN(xp)) {
+             console.log("Por favor, insira um número válido para a experiência (XP).");
+         } else {
+             let nivel = determinarNivel(xp);
+             console.log(`O Herói de nome ${nome} está no nível de ${nivel}.`);
+         }
+
+         
+         continuar = await perguntarContinuar();  
+        
+    }
+
+    console.log("Encerrando a aplicação...");
+    rl.close(); 
+}
+
+classificarHerois();
